ebaa2d6bc6e8bfdaf6134b32e23ac9505852bf3e
Betulilgi <betullilgiiim@gmail.com>
2022-07-29 10:00:38 +0300

    Kişi ekleme, silme ve güncelleme işlemleri

---
+++ Director/Controllers/HomeController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration; //web.config'den bağlantı dizesini çağırmak için bir ad alanı ekledik.
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Director.Controllers
+{
+    public class HomeController : Controller
+    {
+        //database bağlantısı
+        String ConnectionString = ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //EKLEME FONKSİYONU
+        public JsonResult InsertFields(string name, string lName, string pNumber, string tc)
+        {
+            //Hata varsa yakalamak için try-catch kullanılır.
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open(); //veritabanı bağlantısını açma
+                SqlCommand cmd = new SqlCommand("insert into person(name,lName, pNumber, tc) values ('" + name + "', '" + lName + "', '" + pNumber + "', '"+ tc + "' )", con);
+                cmd.ExecuteNonQuery();  //veritabanındaki verileri kullanmadan DataSet değiştirmek için.
+                con.Close();
+
+                return Json("success", JsonRequestBehavior.AllowGet);   //Json işlevi olduğundan dönüş tipi de json olmalıdır.
+            }
+            catch (Exception ex)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //VERİLERİ EKRANDA GÖSTER
+        public JsonResult ViewData()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM person", con);
+                DataTable dt = new DataTable();
+                SqlDataAdapter cmd1 = new SqlDataAdapter(cmd);
+                cmd1.Fill(dt);
+                string html = "";
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        html += "['" + dt.Rows[i]["id"] + "','" + dt.Rows[i]["name"] + "', '" + dt.Rows[i]["lName"] + "', '" + dt.Rows[i]["pNumber"] + "' , '" + dt.Rows[i]["tc"] + "', " +
+                            "'<a onclick=fnEdit(" + dt.Rows[i]["id"] + ")> <b> Düzenle </b></a>  /  <a onclick= fnDelete(" + dt.Rows[i]["id"] + ") <b> Sil </b></a>' ],";
+                    }
+                }
+                con.Close();
+                html = html.Substring(0, Math.Max(0, html.Length - 1));
+
+                return Json(new { html }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //SİLME FONKSİYONU
+        public JsonResult DeleteData(string id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from person where id = '" + id + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                return Json("success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //DÜZENLEME FONKSİYONU
+        public JsonResult EditData(string id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT name,lName,pNumber,tc,id FROM person WHERE id = '" + id + "' ", con);
+                DataTable dt = new DataTable();
+                SqlDataAdapter cmd1 = new SqlDataAdapter(cmd);
+                cmd1.Fill(dt);
+
+                string[] htmlValues = new string[dt.Columns.Count];
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        htmlValues[i] = dt.Rows[0].ItemArray[i].ToString();
+                    }
+                }
+                con.Close();
+
+                return Json(new { htmlValues }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //GÜNCELLE FONKSİYONU
+        public JsonResult UpdateFields(string name, string lName, string pNumber, string tc, int id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Update person set name = '" + name + "', lName = '" + lName + "', pNumber = '" + pNumber + "',tc = '" + tc + "' Where id = '" + id+"'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                return Json("success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Director/Scripts/CRUD/Crud.js
@@ -0,0 +1,160 @@
+﻿function fnInsert() {
+    var name = $("#name").val();
+    var lName = $("#lName").val();
+    var pNumber = $("#pNumber").val();
+    var tc = $("#tc").val();
+
+
+    if (tcController(tc)) {
+        //Ajax metodu kullanarak gönderdiğimiz veriyi database'e kaydedeceğiz.
+        $.ajax({
+            type: "POST",
+            url: "/Home/InsertFields/",    //url bir denetleyciyi aramak için kullanılır. Home denetleyicisini çağırıyoruz.
+            datatype: "json",
+            contentType: "application/json: charset=utf-8",
+            data: JSON.stringify({ name: name, lName: lName, pNumber: pNumber, tc: tc }),   //index.html deki verileri kullanılır.
+            success: function (json1) {
+                window.location.replace("/Home/Index/");
+                ViewData();
+            },
+            failure: function (errMsg) {
+                alert(errMsg);
+            }
+        });
+    } else {
+        alert("T.C kimlik numarası geçersizdir.");
+    }
+    
+}
+function tcController(tc) {
+    var tekToplam = 0, ciftToplam = 0, toplam = 0;
+    for (var i = 0; i < 9; i++) {
+        if (i % 2 == 0) {
+            tekToplam += Number(tc[i]);
+        } else {
+            console.log(tc[i])
+            ciftToplam += Number(tc[i]);
+        }
+        toplam += Number(tc[i]);
+    }
+    console.log(tekToplam);
+    console.log(ciftToplam);
+    console.log(toplam);
+    console.log(Number(tc[9]));
+    tekToplam *= 7;
+    console.log(tc[0] == 0);
+    console.log((tekToplam - ciftToplam) % 10)
+    console.log(tc[9])
+    console.log(((toplam + tc[9]) % 10));
+    
+
+
+    if (tc[0] == 0 || tc[9] != (tekToplam - ciftToplam) % 10 || ((toplam + Number(tc[9])) % 10) != tc[10] || tc.length < 11) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+$(document).ready(function () { //bu fonksiyon sayfa yüklendiğinde yüklenir.
+    ViewData();
+    $("#update").hide();
+    function ViewData() {
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/ViewData/",
+            datatype: "json",
+            contentType: "application/json: charset=utf-8",
+            data: JSON.stringify({}),
+        success: function (json1) {
+            /*debugger*/
+            var tableload = json1.html;
+            var dataset = eval("[" + tableload + "]");
+            $('#ViewValues').DataTable({
+                retrieve: true, //
+
+                ordering: false,
+                data: dataset,
+                columns: [
+                    { title: "ID" },
+                    { title: "Ad" },
+                    { title: "Soyadı" },
+                    { title: "Telefon Numarası" },
+                    { title: "T.C.No" },
+                    { title: "Durum" }, //Delete, Edit
+                ]
+            });
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }      
+        });
+    }
+});
+
+
+
+function fnDelete(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Home/DeleteData/",
+        datatype: "json",
+        contentType: "application/json: charset=utf-8",
+        data: JSON.stringify({ id :id }),
+        success: function (json) {
+            window.location.replace("/Home/Index/");
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+}
+
+function fnEdit(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Home/EditData/",
+        datatype: "json",
+        contentType: "application/json: charset=utf-8",
+        data: JSON.stringify({ id: id }),
+        success: function (json) {
+            var arrval = json.htmlValues;
+            $("#name").val(arrval[0]);
+            $("#lName").val(arrval[1]);
+            $("#pNumber").val(arrval[2]);
+            $("#tc").val(arrval[3]);
+            $("#id").val(arrval[4]);
+
+            $("#Btn").hide();
+            $("#update").show();
+
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+}
+
+function fnUpdate() {
+    var name = $("#name").val();
+    var lName = $("#lName").val();
+    var pNumber = $("#pNumber").val();
+    var tc = $("#tc").val();
+    var id = $("#id").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/UpdateFields/",
+        datatype: "json",
+        contentType: "application/json: charset=utf-8",
+        data: JSON.stringify({ name: name, lName: lName, pNumber: pNumber, tc: tc, id: id}),
+        success: function (json1) {
+            ViewData();
+            window.location.replace("/Home/Index/");
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+}
